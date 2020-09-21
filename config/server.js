c68a4475e41516953eb275de45e301d963bb3561
florianGierlichs <gierlichs@d-sire.com>
2020-09-21 17:25:07 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd2ad855c8fbd65881fc1503889f3a5c7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c956d9a2-04a8-4d0a-b295-4ddab9955fd2'
+};
